a9123c16413a6cf86c0a05d3ac372a2c58ce7d8f
Arturo Paz <arturopazp@gmail.com>
2025-01-05 09:18:14 -0500

    se agregó el ultimo archivo del capitulo 3

---
+++ 3-11.py
@@ -0,0 +1,6 @@
+# 3.11 List index out of range
+cosas = ['salud','dinero','amor','aprendizaje','ciclismo','networking','viajar','trabajo','paz']
+
+print(f"La lista tiene {len(cosas)} elementos")
+print(f"Se tratara de acceder a un elemento {len(cosas) + 1 }")
+#print(cosas[9])
